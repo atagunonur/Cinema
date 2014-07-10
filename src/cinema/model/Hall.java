a19b042631830f252d9439819a784f914af15038
atagunonur <atagun.onur@gmail.com>
2014-07-10 13:04:09 +0300

    Revert "Revert "add ticket model""
    This reverts commit c9015194850df5b35255148d387ecd2cf0da24d0.

--- src/cinema/model/Hall.java
+++ src/cinema/model/Hall.java
@@ -5,26 +5,28 @@ public class Hall {
 	private int rows;
 	private int cols;
 	private String name;
-	
+
 	public Hall(int rows, int cols, String name) {
 		this.rows = rows;
 		this.cols = cols;
 		this.name = name;
 	}
+
 	public int getRows() {
 		return rows;
 	}
+
 	public int getCols() {
 		return cols;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	@Override
 	public String toString() {
 		return "Hall [id=" + id + ", rows=" + rows + ", cols=" + cols
 				+ ", name=" + name + "]";
 	}
-
-	
 }
